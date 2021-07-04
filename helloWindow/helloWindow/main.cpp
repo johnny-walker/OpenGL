d78e1f54f245061e9259b4e224bf101cc9c72315
johnny-walker <albert_hsu@cyberlink.com>
2021-07-04 19:31:20 +0800

    add hello Triangle

--- helloWindow/helloWindow/main.cpp
+++ helloWindow/helloWindow/main.cpp
@@ -92,5 +92,5 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
-    cout << width << 'x' <<height << endl;
+    cout << width << ',' <<height << endl;
 }
